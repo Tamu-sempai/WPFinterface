6775942134579eb852b9b0424eb838e8881efb91
Tamu-sempai <veronikakiseleva86@gmail.com>
2022-12-25 22:09:24 +0500

    first commit

---
+++ GraphicsEditor/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphicsEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RadioButton btnPen = new RadioButton();
+        RadioButton btnBrush = new RadioButton();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ComboBox_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void cmb_RED_SelectionChanged(object sender, RoutedEventArgs e)//
+        {
+            MyCanvas.DefaultDrawingAttributes.Color = Colors.Red;
+        }
+        private void cmb_Green_SelectionChanged(object sender, RoutedEventArgs e)//
+        {
+            MyCanvas.DefaultDrawingAttributes.Color = Colors.Green;
+        }
+        private void cmb_Blue_SelectionChanged(object sender, RoutedEventArgs e)//
+        {
+            MyCanvas.DefaultDrawingAttributes.Color = Colors.Blue;
+        }
+        private void cmb_Violet_SelectionChanged(object sender, RoutedEventArgs e)//
+        {
+            MyCanvas.DefaultDrawingAttributes.Color = Colors.Violet;
+        }
+        private void cmb_Brown_SelectionChanged(object sender, RoutedEventArgs e)//
+        {
+            MyCanvas.DefaultDrawingAttributes.Color = Colors.Brown;
+        }
+        private void cmb_Khaki_SelectionChanged(object sender, RoutedEventArgs e)//
+        {
+            MyCanvas.DefaultDrawingAttributes.Color = Colors.Khaki;
+        }
+        private void SliderPen(object sender, RoutedEventArgs e)
+        {
+            if (MyCanvas == null)
+            {
+                return;
+            }
+            MyCanvas.DefaultDrawingAttributes.Width = WidthPen.Value;
+            MyCanvas.DefaultDrawingAttributes.Height = WidthPen.Value;
+        }
+        private void RadioButton_Erase(object sender, RoutedEventArgs e)
+        {
+            MyCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+        private void RadioButton_Pen(object sender, RoutedEventArgs e)
+        {
+            MyCanvas.EditingMode = InkCanvasEditingMode.Ink;
+
+        }
+        private void RadioButton_Highl(object sender, RoutedEventArgs e)
+        {
+            MyCanvas.EditingMode = InkCanvasEditingMode.Select;
+
+        }
+        private void RadioButton_Delet(object sender, RoutedEventArgs e)
+        {
+            MyCanvas.Strokes.Clear();
+        }
+    }
+}
---
+++ MenuWPF/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MenuWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RED_Click(object sender, RoutedEventArgs e)
+        {
+            Window.Background = new SolidColorBrush(Colors.LightSalmon);
+        }
+        private void GREEN_Click(object sender, RoutedEventArgs e)
+        {
+            Window.Background = new SolidColorBrush(Colors.Olive);
+        }
+        private void BLUE_Click(object sender, RoutedEventArgs e)
+        {
+            Window.Background = new SolidColorBrush(Colors.SkyBlue);
+        }
+        private void VIOLET_Click(object sender, RoutedEventArgs e)
+        {
+            Window.Background = new SolidColorBrush(Colors.MediumPurple);
+        }
+        private void YOU_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Киселева Вероника Алексеевна\nПР-31", "О разработчике", MessageBoxButton.OK, MessageBoxImage.Question);
+        }
+        private void Menu_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            CursorPosition.Text = "Изменение окна...";
+        }
+        private void Close_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            CursorPosition.Text = "Закрыть окно...";
+        }
+        private void YOU_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            CursorPosition.Text = "Информация о разработчике...";
+        }
+        private void Color_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            CursorPosition.Text = "Изменить цвет окна...";
+        }
+        private void ToolBarColor_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            CursorPosition.Text = "Изменить цвет окна...";
+        }
+        private void ToolBarYOU_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            CursorPosition.Text = "Информация о разработчике...";
+        }
+        private void ToolBarClose_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            CursorPosition.Text = "Закрыть окно...";
+        }
+        private void Button_MouseLeave(object sender, RoutedEventArgs e)
+        {
+            CursorPosition.Text = "";
+        }
+    }
+}
